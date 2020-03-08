a3bcf679c3c625d6862607333cd203dd771a1412
bdewhirst <b.dewhirst@gmail.com>
2020-03-08 10:08:01 -0400

    Create minimal 'hello world'

--- main/app.py
+++ main/app.py
@@ -1,1 +1,19 @@
-# TODO
+import flask
+
+
+app = flask.Flask(__name__)
+
+
+@app.route("/")
+def main():
+    """
+    For now, this is a flask 'hello world'
+    
+        example: 
+        - open http://localhost:5000/ in a browser
+    """
+    return "Hello World!"
+
+
+if __name__ == "__main__":
+    app.run()
